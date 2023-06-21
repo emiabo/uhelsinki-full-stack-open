2bcb4a25580dcc92a4312c307a2ad470d685b38b
Emilia Aboudara <hello@emiabo.com>
2023-06-21 16:18:37 -0700

    exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -19,7 +19,7 @@ const Feedback = (props) => {
 
 const Display = (props) => {
   return(
-    <p>{props.text} {props.rating}</p>
+    <p>{props.text} {props.value} {props.suffix}</p>
   )
 }
 
@@ -27,9 +27,12 @@ const Statistics = (props) => {
   return(
     <div>
       <h2>statistics</h2>
-      <Display text='good' rating={props.good} />
-      <Display text='neutral' rating={props.neutral} />
-      <Display text='bad' rating={props.bad} />
+      <Display text='good' value={props.good} />
+      <Display text='neutral' value={props.neutral} />
+      <Display text='bad' value={props.bad} />
+      <Display text='all' value={props.total} />
+      <Display text='average' value={props.avg} />
+      <Display text='positive' value={props.percentGood} suffix='%'/>
     </div>
   )
 }
@@ -39,11 +42,47 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [avg, setAvg] = useState(0.0)
+  const [percentGood, setPercentGood] = useState(0.0)
+
+  const addGood = () => {
+    const newGood = good+1
+    const newTotal = total+1
+    const newAvg = (newGood-bad)/newTotal
+    const newPercentGood = (newGood/newTotal)*100
+    setGood(newGood)
+    setTotal(newTotal)
+    setAvg(newAvg)
+    setPercentGood(newPercentGood)
+  }
+
+  const addNeutral = () => {
+    const newNeutral = neutral+1
+    const newTotal = total+1
+    const newAvg = (good-bad)/newTotal
+    const newPercentGood = (good/newTotal)*100
+    setNeutral(newNeutral)
+    setTotal(newTotal)
+    setAvg(newAvg)
+    setPercentGood(newPercentGood)
+  }
+
+  const addBad = () => {
+    const newBad = bad+1
+    const newTotal = total+1
+    const newAvg = (good-newBad)/newTotal
+    const newPercentGood = (good/newTotal)*100
+    setBad(newBad)
+    setTotal(newTotal)
+    setAvg(newAvg)
+    setPercentGood(newPercentGood)
+  }
 
   return (
     <div>
-      <Feedback setGood={() => setGood(good+1)} setNeutral={() => setNeutral(neutral+1)} setBad={() => setBad(bad+1)} />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Feedback setGood={addGood} setNeutral={addNeutral} setBad={addBad} />
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} avg={avg} percentGood={percentGood} />
     </div>
   )
 }
