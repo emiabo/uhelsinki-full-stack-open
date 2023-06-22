ab33f72e78b8381756404f02d41888139e1d5e95
Emilia Aboudara <hello@emiabo.com>
2023-06-22 12:13:32 -0700

    Exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -19,7 +19,10 @@ const Feedback = (props) => {
 
 const StatisticLine = (props) => {
   return(
-    <p>{props.text} {props.value} {props.suffix}</p>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value} {props.suffix}</td>
+    </tr>
   )
 }
 
@@ -35,12 +38,16 @@ const Statistics = (props) => {
   return(
     <div>
       <h2>statistics</h2>
-      <StatisticLine text='good' value={props.good} />
-      <StatisticLine text='neutral' value={props.neutral} />
-      <StatisticLine text='bad' value={props.bad} />
-      <StatisticLine text='all' value={props.total} />
-      <StatisticLine text='average' value={props.avg} />
-      <StatisticLine text='positive' value={props.percentGood} suffix='%'/>
+      <table>
+        <tbody>
+          <StatisticLine text='good' value={props.good} />
+          <StatisticLine text='neutral' value={props.neutral} />
+          <StatisticLine text='bad' value={props.bad} />
+          <StatisticLine text='all' value={props.total} />
+          <StatisticLine text='average' value={props.avg} />
+          <StatisticLine text='positive' value={props.percentGood} suffix='%'/>
+        </tbody>
+      </table>
     </div>
   )
 }
